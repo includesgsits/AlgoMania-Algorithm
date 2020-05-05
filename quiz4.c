cf22df967fd03c1dfa4892e8f0f0c3a5f13d27b2
Kanak Agrawal <kanakagrawal600@gmail.com>
2020-05-05 13:45:55 +0530

    Added Quiz 4

---
+++ quiz4.c
@@ -0,0 +1,34 @@
+
+  //here n is size of array.
+    
+int algo(int arr[], int n, int x) 
+{ 
+    int i = 1; 
+    if (arr[0] == x) 
+        return 0; 
+  
+    while (i < n && arr[i] <= x) 
+        i = i*2; 
+  
+   return subAlgo(arr, i/2, min(i, n), x); 
+} 
+  
+
+int subAlgo(int arr[], int l, int r, int x) 
+{ 
+    if (r >= l) 
+    { 
+        int num = l + (r - l)/2; 
+  
+        if (arr[num] == x) 
+            return num; 
+  
+        if (arr[num] > x) 
+            return subAlgo(arr, l, num-1, x); 
+  
+    return subAlgo(arr, num+1, r, x); 
+    } 
+  
+  return -1; 
+} 
+
