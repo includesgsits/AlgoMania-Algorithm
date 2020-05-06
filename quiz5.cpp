712e152e506ce650bce869688dfde8e13e0513a1
Piyush <mpiyush484@gmail.com>
2020-05-06 10:08:33 +0530

    added quiz 5

--- quiz5.cpp
+++ quiz5.cpp
@@ -19,23 +19,20 @@ int main()
     for (int i = 0; i < x; i++)  c[(int)b[i]] = i;  
   
     int d = 0; 
-    while(d <= (y - x))  
-    {  
+    while(d <= (y - x))  {  
         int j = x - 1;  
-        
         while(j >= 0 && b[j] == a[d + j])  j--;  
-
         if (j < 0)  
         {  
             ans.push_back(d);  
             if((d+x) < y) d += x - c[(int)a[d+x]];
-			else d+=1;
-        }  
-  
+	    else d+=1;
+        }
         else d += max(1, j - c[(int)a[d + j]]);  
     }  
     
     for(auto i: ans) cout<<i<<" ";
-	cout<<"\n";
+
+    cout<<"\n";
 return 0; 
 }
